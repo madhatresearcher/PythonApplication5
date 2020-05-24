baaa7991fe657b03514e886e67717fa817522006
Jurugo Felix <jurugofelix123@live.com>
2020-05-24 15:40:18 +0300

    Add project files.

---
+++ PythonApplication5/PythonApplication5.py
@@ -0,0 +1,18 @@
+amazon_cart=['laptop','shoes','phone',6,'trolley'];
+print('enter the extra items to buy:');
+item1=input();
+item2=input();
+item3=input();
+item4=input();
+item5=input();
+newegg_cart=[item1,item2,item3,item4,item5];
+print(newegg_cart);
+print(len(newegg_cart));
+print(newegg_cart[0:len(newegg_cart)]);
+print(type(item1));
+newegg_cart[0]='brakes';
+print(newegg_cart);
+newegg_cart[4]='shoe pads';
+newegg_cart[3]=90;
+newegg_cart=newegg_cart+['snakes'];
+print(newegg_cart);
